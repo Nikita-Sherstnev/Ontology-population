e1cab3ce213fbb3d5537a91a558aca101765d01c
Nikita-Sherstnev <sherstnyov@vivaldi.net>
2020-07-09 00:15:07 +0400

    Test refactored

---
+++ test/test_nlp.py
@@ -0,0 +1,13 @@
+import unittest
+from main import nlp
+
+class TestNLPMethods(unittest.TestCase):
+
+    def test_normalization(self):
+        text = [['Июль', 'был', 'жарким', '.'],['Весна', '-', 'теплой', '.']]
+
+        self.assertEqual(nlp.normalization(text), [['июль', 'жарким'],['весна', 'теплой']])
+
+
+if __name__ == '__main__':
+    unittest.main()
